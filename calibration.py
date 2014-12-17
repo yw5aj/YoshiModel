912b2f919e8f7fdf2ee25235ba06e12e8d115668
Yuxiang Wang <yw5aj@virginia.edu>
2014-12-16 22:27:32 -0500

    Tried to fix a name clash but failed

--- calibration.py
+++ calibration.py
@@ -4,7 +4,7 @@ from abqimport import *
 
 
 def getFitFem(runFiber=True, doAnalysis=True):
-    from constants import materialBlockFiber as materialBlock
+    from feconstants materialBlockFiber as materialBlock
     stimBlock = {'rampLiftTimeArray': .4*np.ones(10), 'holdDisplArray': np.r_[25:250:10j]*1e-3}
     from setfiber import Fiber
     fiber = Fiber(baseModelName='FitFem', suffix='', stimBlock=stimBlock, materialBlock=materialBlock, runFiber=runFiber, doAnalysis=doAnalysis, skipWait=False)
