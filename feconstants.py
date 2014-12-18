73f0e644265f5dccc1b366189a47987ae8cee2b6
Yuxiang Wang <yw5aj@virginia.edu>
2014-12-17 22:30:52 -0500

    Changed skin thickness back to minimal

--- feconstants.py
+++ feconstants.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 # Set default stim block for displ
 _displtimecoeff = np.loadtxt('./csvs/displtimecoeff.csv', delimiter=',')
 stimBlockDefault = {'holdDisplArray': np.r_[.032:.16:5j]}
@@ -14,7 +15,6 @@ materialBlockDefault = {'thicknessAll': [418.5, 338.8, 10.1348], 'skin_g_array':
     'skin_tau_array': [np.inf, 1.111, .092], 'skin_mu': 6.354e3, 'skin_alpha': 8.787,
     'sylgard_c10': 1.05e5, 'sylgard_g': 0.03, 'sylgard_tau': 0.7}
 # Try a new skin to fit the transduction function
-# Parameters: 0.060315611	0.725972449	0.473834383	0.0757945	0.450371116	1199.757277	7.707131248	0.898151475	0.4239571	0.328328	27	0.195									
-materialBlockFiber = {'thicknessAll': [328.33, 338.8, 10.1348], 'skin_g_array': [0.474, 0.076, 0.450], 
-    'skin_tau_array': [np.inf, 0.060, 0.726], 'skin_mu': 1199.757 * 7.707 / 2., 'skin_alpha': 7.707,
-    'sylgard_c10': 1.05e5, 'sylgard_g': 0.03, 'sylgard_tau': 0.7}
+skinThickArray, skinAlphaArray, sylgardThickArray, sylgardC10Array, g1Array, g2Array, ginfArray = np.genfromtxt('./csvs/simprop.csv', delimiter=',').T
+materialBlockFiber = copy.deepcopy(materialBlockDefault)
+materialBlockFiber['thicknessAll'][0] = skinThickArray[0]
