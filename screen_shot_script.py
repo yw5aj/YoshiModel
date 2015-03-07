6d72b3dc5d02b11e63b1263307e0d71b171c01db
Yuxiang Wang <yw5aj@virginia.edu>
2015-03-07 11:51:05 -0500

    Modified screen shot to other spectrums

--- screen_shot_script.py
+++ screen_shot_script.py
@@ -15,10 +15,20 @@ session.viewports['Viewport: 1'].view.setValues(nearPlane=0.0377807,
 session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(
     compass=OFF, title=OFF, state=OFF, annotations=OFF, legendBox=OFF,
     legendNumberFormat=FIXED)
-# session.viewports['Viewport: 1'].odbDisplay.contourOptions.setValues(
-    # contourStyle=CONTINUOUS, spectrum='Reversed rainbow')
+# Setting contour and edge colors here
+color_list = []
+for i in range(16):
+    color_list.append('#%sFF' % (hex(15-i)[-1]*4))
+
+for i in range(15):
+    color_list.append('#0000%s' % (hex(14-i)[-1]*2))
+
+# session.Spectrum(name="Blue", colors = tuple(['#%sFF' % (hex(i)[-1]*4) for i in range(15)]))
+session.Spectrum(name="Blue", colors = tuple(color_list[::-1]))
 session.viewports['Viewport: 1'].odbDisplay.contourOptions.setValues(
     contourStyle=CONTINUOUS, spectrum='Black to white')
+session.viewports['Viewport: 1'].odbDisplay.contourOptions.setValues(
+    edgeColorBandedQuilt='#000000') 
 session.viewports['Viewport: 1'].odbDisplay.setPrimaryVariable(
     variableLabel='S', outputPosition=INTEGRATION_POINT, refinement=(INVARIANT, 
     'Min. Principal'), )
@@ -45,6 +55,7 @@ session.printToFile(
 session.printToFile(
     fileName='X:/WorkFolder/AbaqusFolder/YoshiModel/figures/screenshot_zoomed_in', 
     format=EPS, canvasObjects=(session.viewports['Viewport: 1'], ))    
+
 
 #%% For understanding stress distribution
 from abaqus import *
