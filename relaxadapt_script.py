cdb782928ac8ba12b594b4a78655a3fcdd44b070
Yuxiang Wang <yw5aj@virginia.edu>
2015-05-06 15:31:38 -0400

    Changed the amount of force of ra same with hmstss

--- relaxadapt_script.py
+++ relaxadapt_script.py
@@ -11,13 +11,21 @@ runFiberForce = True
 doAnalysisForce = True
 skipWait = True
 
-def runSimulation(baseModelName, materialBlock):
+def runSimulation(baseModelName, materialBlock, stimBlockForce):
     # Run displacement controlled analysis
     fiber = Fiber(baseModelName=baseModelName, materialBlock=materialBlock, runFiber=runFiberDispl,
         doAnalysis=doAnalysisDispl, skipWait=skipWait)
     # Now to force controlled
-    fiberForce = FiberForce(fiber, runFiber=runFiberForce, doAnalysis=doAnalysisForce, skipWait=skipWait)
+    fiberForce = FiberForce(fiber, stimBlock=stimBlockForce, runFiber=runFiberForce, doAnalysis=doAnalysisForce, skipWait=skipWait)
     return
+
+    
+# Set force array
+_displtimecoeff = np.loadtxt('./csvs/displtimecoeff.csv', delimiter=',')
+stimBlockForce = {'holdForceArray': np.r_[1.5e-3:7.5e-3:5j]}
+_displforce = np.loadtxt('./csvs/FitFemDisplforce.csv', delimiter=',')
+eqdisp = np.interp(stimBlockDefaultForce['holdForceArray'], _displforce[:, 1], _displforce[:, 0]*1e3)
+stimBlockForce['rampLiftTimeArray'] = np.polyval(_displtimecoeff, eqdisp)
 
     
 skinThickArray, skinAlphaArray, sylgardThickArray, sylgardC10Array, g1Array, g2Array, ginfArray = np.genfromtxt(
@@ -32,7 +40,7 @@ for level, (raThick, raThickG1, raThickG2, raThickGinf) in enumerate(zip(raThick
     materialBlock = copy.deepcopy(materialBlockDefault)
     materialBlock['thicknessAll'][0] = raThick
     materialBlock['skin_g_array'] = [raThickGinf, raThickG2, raThickG1]
-    runSimulation('RaThick%d'%level, materialBlock)
+    runSimulation('RaThick%d'%level, materialBlock, stimBlockForce)
 
 
 # Get distribution csvs
